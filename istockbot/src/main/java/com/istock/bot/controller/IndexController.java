27fc5fc67445a7c2a639f200498f49e47cdf8220
kalisma84@gmail.com <kalisma84@gmail.com@7211355b-1195-7356-961a-6218de360b6c>
2014-06-03 15:43:36 +0000

    kalisma84 : change for test.

--- istockbot/src/main/java/com/istock/bot/controller/IndexController.java
+++ istockbot/src/main/java/com/istock/bot/controller/IndexController.java
@@ -69,39 +69,34 @@ public class IndexController {
 		} else if(action.equals("sendFromMessage")) {
 			System.out.println(request.getParameter("groupId"));
 		} else if(action.equals("sendFromGroup")) {
-			System.out.println(request.getParameter("groupId"));
+			ParserScheduler ps = new ParserScheduler();
+			
 			String content = request.getParameter("content");
 			if(content.contains("/r:")) {
-				ParserScheduler ps = new ParserScheduler();
 				indexService.sendMsg("- 글번호 " + content.split(":")[1] + " 파싱시작 -");
 				ps.resetCommentNum();
 				ps.setActiveSchedule(true);
 				ps.setArticleId(content.split(":")[1]);
 			}
 			if(content.contains("/c:")) {
-				ParserScheduler ps = new ParserScheduler();
 				//indexService.sendMsg("- 종목코드 " + content.split(":")[1] + " 파싱시작 -");
 				ps.searchCode(content.split(":")[1]);
 			}
 			if(content.contains("/n:")) {
-				ParserScheduler ps = new ParserScheduler();
 				ps.searchName(content.split(":")[1]);
 			}
 			if(content.equals("/s")){
-				ParserScheduler ps = new ParserScheduler();
 				indexService.sendMsg("- 최근글로 파싱시작 -");
 				ps.resetCommentNum();
 				ps.setActiveSchedule(true);
 				ps.printForCallback();
 			}
 			if(content.equals("/a")){
-				ParserScheduler ps = new ParserScheduler();
 				indexService.sendMsg("- 동작실행 -");
 				ps.resetCommentNum();
 				ps.setActiveSchedule(true);
 			}
 			if(content.equals("/q")){
-				ParserScheduler ps = new ParserScheduler();
 				indexService.sendMsg("- 동작중지 -");
 				ps.setActiveSchedule(false);
 			}
